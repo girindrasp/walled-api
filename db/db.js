1ebf49f3bc75e1f44fcba2ff009a3ca445ce5a55
Girindra Syukran Prahasto <girindra220202@gmail.com>
2024-12-18 14:40:03 +0700

    chore: added .env

--- db/db.js
+++ db/db.js
@@ -2,11 +2,6 @@ const { connectionString } = require("pg/lib/defaults");
 
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "walled",
-  password: "postgres",
-  port: 5432,
   connectionString: process.env.DB_URL
 });
 
--- utils/auth.util.js
+++ utils/auth.util.js
@@ -1,7 +1,6 @@
 const jwt = require("jsonwebtoken");
 
 function generateAccessToken(username) {
-  const TOKEN_SECRET = "secret";
   return jwt.sign(username, TOKEN_SECRET, { expiresIn: "24h" });
 }
 
